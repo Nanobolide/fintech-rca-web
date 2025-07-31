9ebdbc2aab88c7f2f5f326f88c28b4d9de487598
Nanobolide <maidougeoffroyjaphet@gmail.com>
2025-07-31 20:00:38 +0000

    Installation de shadcn et le logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './index.css';
+import { Button } from "@/components/ui/button"
 
 const App = () => {
   return (
     <div>
           <h1 className="text-3xl font-bold underline">
-               Hello world!
+               Hello world!<Button>Click toi</Button>
+               <div className="flex min-h-svh flex-col items-center justify-center">
+             <Button>Click me</Button>
+    </div>
       </h1>
     </div>
   )
